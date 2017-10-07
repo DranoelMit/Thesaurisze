f53ac1c72dfe4310185bf2182b49e0dbab6f1973
DranoelMit <tim.matthew.leonard@gmail.com>
2017-10-07 14:32:34 -0400

    Add conversion from html input to array of words

--- wordSwapper.js
+++ wordSwapper.js
@@ -1,13 +1,35 @@
-function findKeywords()
+function findKeywords(sentence)
 {
-var sentecne = document.getElementById("sentence");
+
+
+ var words =[];
+var startWord =0;
+for (var i = 0; i < sentence.length; i++) {
+      if(sentence.indexOf(" ") == -1){ words[0] = sentence; break;}
+
+if(sentence.charAt(i)== ' '){
+words.push(sentence.substring(startWord, i));
+startWord=i+1;
 }
+
+else if (i==sentence.length-1)
+  words.push(sentence.substring(startWord));
+}
+
+
+
+for (var i = 0; i < words.length; i++) {
+
+}
+
+}
+
 
 
 function replaceKeyword(word){
   var xhr = new XMLHttpRequest();
   xhr.open("GET",
-          "http://words.bighugelabs.com/9e144e42dacfd6413f3d7eb94aed5017"+word+"/",
+          "http://words.bighugelabs.com/api/2/9e144e42dacfd6413f3d7eb94aed5017"+word+"/xml",
           false);
   xhr.send();
 }
